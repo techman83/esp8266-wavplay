c3808d5080510e466812a59de191325f3fd5dc72
DavideRuzza <30658668+DavideRuzza@users.noreply.github.com>
2021-09-21 22:22:40 +0200

    Added setGain

--- ESP8266Wavplay.h
+++ ESP8266Wavplay.h
@@ -1,9 +1,12 @@
+
+
 #ifndef __ESP8266WAVPLAY_H__
 
 #include "Arduino.h"
 #include "i2s.h"
 #include "i2s_reg.h"
 #include "ESP8266Wavspiffs.h"
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -15,6 +18,8 @@ bool wavPlaying();
 void wavStopPlaying();
 void wavLoop();
 void showDir();
+int16_t Amplify(int16_t s);
+bool setGain(float f);
 
 #ifdef __cplusplus
 }
